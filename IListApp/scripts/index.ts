721070dd5332f8b8ebad5f2189ada57901bc1297
Sagi Karni <sagikarni76@gmail.com>
2016-01-31 23:06:13 +0200

    test

--- IListApp/scripts/index.ts
+++ IListApp/scripts/index.ts
@@ -19,13 +19,36 @@ module IListApp {
             document.addEventListener('pause', onPause, false);
             document.addEventListener('resume', onResume, false);
 
-            // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-            AppSettings.browser = window.open('http://192.168.1.16:9876/default.aspx', '_blank', 'titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no');
             navigator.splashscreen.show();
-            AppSettings.browser.addEventListener("loadstop", function () {
-                navigator.splashscreen.hide();
-                pollForSocialShare();
-            });
+
+            var iframe = document.createElement('iframe');
+            iframe.src = "http://192.168.1.11:9876/default.aspx";
+            iframe.width = window.outerWidth.toString();
+            iframe.height = window.outerHeight.toString();
+            iframe.id = "iframe";
+
+            document.body.appendChild(iframe);
+
+
+            var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+            var eventer = window[eventMethod];
+            var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+
+            // Listen to message from child window
+            eventer(messageEvent, function (e) {
+                console.log('parent received message!:  ', e.data);
+                if (e.data == "share")
+                    (<any>(window.plugins)).socialsharing.share('My message');
+                else
+                    navigator.splashscreen.hide();
+            }, false);
+            // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+            //AppSettings.browser = window.open('http://192.168.1.11:9876/default.aspx', '_blank', 'titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no');
+            //navigator.splashscreen.show();
+            //AppSettings.browser.addEventListener("loadstop", function () {
+            //    navigator.splashscreen.hide();
+            //    pollForSocialShare();
+            //});
         }
 
         function pollForSocialShare() {
